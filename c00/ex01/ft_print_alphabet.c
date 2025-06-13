efa7a5114681fb488badaeb445fa45fb2e8e7ec9
bramz <salahdineouhsain@gmail.com>
2025-06-13 20:30:27 +0100

    add ex01

---
+++ c00/ex01/ft_print_alphabet.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <unistd.h>
+
+void ft_print_alphabet()
+{
+ short countor = 97;
+ char c = countor;
+
+  while(countor <= 122)
+ {
+  c = countor;
+
+  write(1,&c,1);
+  countor++;
+ }
+
+}
+
+int main()
+{
+
+ ft_print_alphabet();
+
+ return 0;
+}
