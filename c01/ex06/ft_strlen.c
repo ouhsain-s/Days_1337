3fe7b42010789ccd2c9788d54b6e2aff3af3e6e7
bramz <salahdineouhsain@gmail.com>
2025-06-15 23:45:37 +0100

    add c01_ex06

---
+++ c01/ex06/ft_strlen.c
@@ -0,0 +1,18 @@
+
+  
+  
+   int ft_strlen(char *str)
+   {
+  
+      short count=0;
+  
+     while(str[count]!='\0')
+      {
+
+
+          count++;
+       }
+     
+       return count;
+
+    }
